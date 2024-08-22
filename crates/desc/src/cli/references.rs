20ab9daaa73588b84043bd7c162aff4c20463868
Blake Sweeney <bsweeney@ebi.ac.uk>
2024-08-22 20:54:04 +0100

    Use the OLS API wrapper to fetch SO and GO term info
    This makes it a lot easier to fetch ontology information. This uses the
    OLS API wrapper to fetch the required information for those databases.

--- crates/desc/src/cli/references.rs
+++ crates/desc/src/cli/references.rs
@@ -1,38 +1,21 @@
 use anyhow::Result;
-use reqwest::blocking::get;
+use openapi::apis::{
+    configuration::Configuration, ontology_term_controller_api::get_term, urlencode,
+};
 use rfam::family::desc::{
     authors::AuthorEdit,
     database_references::{DatabaseReference, XrefEdit},
     desc_file::Edit,
     references::ReferenceEdit,
 };
-use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Eq, PartialEq, PartialOrd, Debug, Hash, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GoInfo {
-    id: String,
-    is_obsolete: bool,
-    name: Option<String>,
-}
-
-#[derive(Clone, Eq, PartialEq, PartialOrd, Debug, Hash, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Response<T> {
-    number_of_hits: usize,
-    results: Vec<T>,
-}
-
-pub fn fetch_go_name(go_id: &str) -> Result<Option<String>> {
-    let url = format!(
-        "https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{}",
-        &go_id
-    );
-    let response: Response<GoInfo> = get(url)?.json()?;
-    match response.results.first() {
-        None => Err(anyhow::anyhow!("No results for {}", &go_id)),
-        Some(r) => Ok(r.name.clone()),
-    }
+pub fn fetch_ols_name(ontology: &str, ont_id: &str) -> Result<Option<String>> {
+    let conf = Configuration::default();
+    let iri = urlencode(format!(
+        "http://purl.obolibrary.org/obo/{ontology}_{ont_id}"
+    ));
+    let term = get_term(&conf, ontology, &iri, None)?;
+    return Ok(term.label);
 }
 
 pub fn add(term: &str) -> Result<Edit> {
@@ -44,9 +27,12 @@ pub fn add(term: &str) -> Result<Edit> {
                     let db_ref = DatabaseReference::new("URL".to_string(), term.to_string(), None);
                     Ok(Edit::Xref(XrefEdit::AddOrUpdate(db_ref)))
                 }
-                "GO" => {
-                    let db_ref =
-                        DatabaseReference::new(db_name, id.to_string(), fetch_go_name(term)?);
+                "GO" | "SO" => {
+                    let db_ref = DatabaseReference::new(
+                        db_name.clone(),
+                        id.to_string(),
+                        fetch_ols_name(&db_name, id)?,
+                    );
                     Ok(Edit::Xref(XrefEdit::AddOrUpdate(db_ref)))
                 }
                 _ => Err(anyhow::anyhow!("Cannot yet fetch from database {}", &db)),
